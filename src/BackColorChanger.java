d886f53be43105d3e8ff4193518d4893b6d09f32
Anas Altaf <f223639@cfd.nu.edu.pk>
2024-10-10 10:28:41 +0500

    add basic project of GUI Login Form

---
+++ src/BackColorChanger.java
@@ -0,0 +1,45 @@
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+public class BackColorChanger extends JFrame implements ActionListener {
+    private JButton redBtn, blueBtn, greenBtn, grayBtn;
+    public BackColorChanger() {
+        setTitle("Color Changer");
+        setLayout(new FlowLayout());
+        redBtn = new JButton("Red");
+        blueBtn = new JButton("Blue");
+        greenBtn = new JButton("Green");
+        grayBtn = new JButton("Gray");
+        redBtn.addActionListener(this);
+        blueBtn.addActionListener(this);
+        greenBtn.addActionListener(this);
+        grayBtn.addActionListener(this);
+        add(redBtn);
+        add(blueBtn);
+        add(greenBtn);
+        add(grayBtn);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setLocation(0, 0);
+        setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == redBtn) {
+            getContentPane().setBackground(Color.RED);
+        } else if (e.getSource() == blueBtn) {
+            getContentPane().setBackground(Color.BLUE);
+        } else if (e.getSource() == greenBtn) {
+            getContentPane().setBackground(Color.GREEN);
+        } else if (e.getSource() == grayBtn) {
+            getContentPane().setBackground(Color.GRAY);
+        }
+    }
+    public static void main(String[] args) {
+        new BackColorChanger();
+    }
+}
